10f0e5b308063502091a44fe94c07619e076c7b5
utthee <yusufalimacalangan@gmail.com>
2025-09-16 23:21:03 +0800

    added modals for colleges page

--- static/script.js
+++ static/script.js
@@ -32,7 +32,6 @@ $("#deleteForm").submit(function(e) {
 });
 
 // ---------- PROGRAMS PAGE ----------
-// SHOW REGISTER CONFIRMATION MESSAGE
 $("#registerForm").submit(function(e) {
     e.preventDefault();
     $("#registerProgramModal").modal('hide');
@@ -54,3 +53,22 @@ $("#deleteForm").submit(function(e) {
 });
 
 // ---------- COLLEGES PAGE ----------
+$("#registerForm").submit(function(e) {
+    e.preventDefault();
+    $("#registerCollegeModal").modal('hide');
+    $("#registerConfirmationModal").modal('show');
+});
+
+//SHOW EDIT CONFIRMATION MESSAGE
+$("#editForm").submit(function(e) {
+    e.preventDefault();
+    $("#editCollegeModal").modal('hide');
+    $("#editConfirmationModal").modal('show');
+});
+
+// SHOW DELETE CONFIRMATION MESSAGE
+$("#deleteForm").submit(function(e) {
+    e.preventDefault();
+    $("#deleteConfirmationModal").modal('hide');
+    $("#deletionModal").modal('show');
+});

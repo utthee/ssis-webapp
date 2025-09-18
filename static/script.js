63fe1f836ac3d7f45a07c4de0bed704aca651450
utthee <yusufalimacalangan@gmail.com>
2025-09-18 23:47:47 +0800

    modified UI for sidebar, dashboard, and pages

--- static/script.js
+++ static/script.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+    $('#data-table').DataTable();
+});
+
 // TOGGLE BUTTON FOR SIDEBAR COLLAPSE/EXPAND
 $(".toggler-btn").click(function() {
     $("#sidebar").toggleClass("collapsed");
@@ -15,6 +19,28 @@ $("#registerForm").submit(function(e) {
     e.preventDefault();
     $("#registerStudentModal").modal('hide');
     $("#confirmationModal").modal('show');
+});
+
+//POPULATE STUDENT EDIT FORM
+$(document).ready(function () {
+  $('#editStudentModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget);
+    var modal  = $(this);
+
+    var idNumber   = button.data('student-id');
+    var firstName  = button.data('first-name');
+    var lastName   = button.data('last-name');
+    var gender     = button.data('gender');
+    var yearLevel  = button.data('year-level');
+    var program    = button.data('program-code');
+
+    modal.find('#idNumber').val(idNumber);
+    modal.find('#firstName').val(firstName);
+    modal.find('#lastName').val(lastName);
+    modal.find('#gender').val(gender);
+    modal.find('#yearLevel').val(yearLevel);
+    modal.find('#programCode').val(program);
+  });
 });
 
 //SHOW EDIT CONFIRMATION MESSAGE
@@ -38,6 +64,22 @@ $("#registerForm").submit(function(e) {
     $("#registerConfirmationModal").modal('show');
 });
 
+//POPULATE PROGRAM EDIT FORM
+$(document).ready(function () {
+  $('#editProgramModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget);
+    var modal  = $(this);
+
+    var programCode = button.data('program-code');
+    var programName = button.data('program-name');
+    var collegeCode = button.data('college-code');
+
+    modal.find('#programCode').val(programCode);
+    modal.find('#programName').val(programName);
+    modal.find('#collegeCode').val(collegeCode);
+  });
+});
+
 //SHOW EDIT CONFIRMATION MESSAGE
 $("#editForm").submit(function(e) {
     e.preventDefault();
@@ -57,6 +99,20 @@ $("#registerForm").submit(function(e) {
     e.preventDefault();
     $("#registerCollegeModal").modal('hide');
     $("#registerConfirmationModal").modal('show');
+});
+
+//POPULATE COLLEGE EDIT FORM
+$(document).ready(function () {
+  $('#editCollegeModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget);
+    var modal  = $(this);
+
+    var code = button.data('college-code');
+    var name = button.data('college-name');
+
+    modal.find('#collegeCode').val(code);
+    modal.find('#collegeName').val(name);
+  });
 });
 
 //SHOW EDIT CONFIRMATION MESSAGE
